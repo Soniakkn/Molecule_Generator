07009ac1f9c1490b119d453e83638e171b156b52
Soniakkn <soniakanojia97@gmail.com>
2026-06-12 10:27:51 +0530

    Update Contact

--- pages/3_Contact_Us.py
+++ pages/3_Contact_Us.py
@@ -6,7 +6,7 @@ st.write("""
 If you have questions, suggestions, or need support regarding the Molecule Generator tool, please reach out using the details below:
 
 - **Email:** soniakanojia97@gmail.com
-- **GitHub:** [Molecule Generator Repository](https://github.com/yourusername/molecule-generator)
+- **GitHub:** [Molecule Generator Repository](https://github.com/Soniakkn/Molecule_Generator/tree/main)
 - **LinkedIn:** [LinkedIN Profile](https://www.linkedin.com/in/sonia-kumari-37653513a/)
 
 We appreciate feedback to improve the tool and make it more useful for the research community.
