587652ee4a81a3cd45afd0a797ebf00378b050f0
Soniakkn <soniakanojia97@gmail.com>
2026-06-19 11:31:17 +0530

    Remove old All_model_files directory

--- All_model_files/save_tokenizer.py
+++
@@ -1,8 +0,0 @@
-# === TOKENIZATION ===
-chars = sorted(set("".join(smiles_list)))
-char2idx = {c: i + 1 for i, c in enumerate(chars)}  # 0 is reserved for padding
-char2idx["<PAD>"] = 0
-idx2char = {i: c for c, i in char2idx.items()}
-
-vocab_size = len(char2idx)
-max_len = max(len(s) for s in smiles_list)
